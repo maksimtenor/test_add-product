2a5f610f25e5e5a4fd0fabeb6c9bd25d150867f2
marevich <maksimrozette@gmail.com>
2021-11-16 21:26:50 +0300

    ubdate 16.11.21, sort card and optymizated load, time to questions - 6 hours

---
+++ main.js
@@ -0,0 +1,126 @@
+// Появление/Исчезание кнопки удаления ".productCard--delete" при наведении на карточку товара
+    $('.productCard--item').mouseenter(function() {
+        $('.item__img').eq($(this).append('<span class="productCard--delete"></span>'));
+});
+    $('.productCard--item').mouseleave(function() {
+        $('.productCard--delete').remove();
+});
+// -------------------------------------------------------------------
+
+// Активация кнопки после введения в инпут данных
+$("input").keyup(function(){
+        if ($('input').val() != '') {
+        $('.form__button').removeAttr('disabled');
+        } else {
+            $('.form__button').attr('disabled','disable');
+        }
+    });
+// -------------------------------------------------------------------
+
+// СОРТИРОВКА КАРТОЧЕК ПО ЦЕНЕ
+
+    // родительскому эленту пропиши в дата-атрибут data-price='10000'
+    // собери все элементы в массив и отсортируй по data-price (через жс)
+
+var downs = $('.productCard--item').sort(function (a, b) {
+
+    var contentA =parseInt( $(a).data('price'));
+    var contentB =parseInt( $(b).data('price'));
+    return (contentA < contentB) ? -1 : (contentA > contentB) ? 1 : 0;
+ });
+ 
+ var ups = $('.productCard--item').sort(function (a, b) {
+
+    var contentA =parseInt( $(a).data('price'));
+    var contentB =parseInt( $(b).data('price'));
+    return (contentA > contentB) ? -1 : (contentA < contentB) ? 1 : 0;
+
+});
+var defaults = $('.productCard--item');
+
+$('#productCard__sort').on('change', function(){
+    
+    if(this.options[this.selectedIndex].value == '0'){
+        return defaults.appendTo($('.block__productCard_items'));
+    }
+    if(this.options[this.selectedIndex].value == '1') {
+        return downs.appendTo($('.block__productCard_items'));
+    }
+    if(this.options[this.selectedIndex].value == '2') {
+        return ups.appendTo($('.block__productCard_items'));
+    }
+
+});
+// -------------------------------------------------------------------
+
+// Валидация Формы
+$('#form__button').on("click",function(e) {
+    console.log('submit is ok');
+    e.preventDefault();
+    var card__name = $('#card__name').val();
+    var card__descriprion = $('#card__descriprion').val();
+    var card__img = $('#card__img').val();
+    var card__price = $('#card__price').val();
+    var border__clear = [card__name,card__descriprion,card__img,card__price];
+
+
+    // Регулярное выражения для проверки ссылок
+    var regEx = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
+    var validLink = regEx.test(card__img);
+
+    $(".error").remove(); // Удаление всех элементов с классом "error"
+
+    // Обнуление красной рамки;
+    for(var border = 0; border <= border__clear.length; border++) {
+        if(border == 0) {
+            $('.card__name').css('border', "none"); 
+                                    console.log(border);
+        }
+        if(border == 1) {
+            $('.card__descriprion').css('border', "none"); 
+                                    console.log(border);
+        }
+        if(border == 2) {
+            $('.card__img').css('border', "none");
+                                    console.log(border);
+        }
+        if(border == 3) {
+            $('.card__price').css('border', "none");
+                                    console.log(border);
+        }
+    }
+
+    // Проверка Названия
+    if (card__name.length< 6) {
+        $('#card__name').after('<span class="error" style="top: 30px; left: 120px; width: 4px; height: 4px; border-radius: 4px; background-color: #FF8484;"></span>');
+        $('#card__name').after('<span class="error" style="top: 88px; left: 24px; color: #FF8484;">Не менее 6 символов</span>');
+        $('.card__name').css('border', "1px solid #FF8484");  
+    }
+    // Проверка Описания
+    if (card__descriprion.length< 20) {
+        $('#card__descriprion').after('<span class="error" style="top: 100px; left: 98px; width: 4px; height: 4px; border-radius: 4px; background-color: #FF8484;"></span>');
+        $('#card__name').after('<span class="error" style="top: 227px; left: 24px; color: #FF8484;">Не менее 20 символов</span>');
+        $('.card__descriprion').css('border', "1px solid #FF8484");  
+    }
+    // Проверка ссылки
+    if (card__img.length< 1) {
+        $('#card__img').after('<span class="error" style="top: 241px; left: 158px; width: 4px; height: 4px; border-radius: 4px; background-color: #FF8484;"></span>');
+    } 
+    // Валидация ссылки
+    if (!validLink) {
+        $('#card__img').after('<span class="error" style="top: 298px; left: 24px;">Поле является обязательным</span>');
+        $('.card__img').css('border', "1px solid #FF8484");   
+        }
+    // Проверка цены 
+    if (card__price.length< 1) {
+        $('#card__price').after('<span class="error" style="top: 310px; left: 77px; width: 4px; height: 4px; border-radius: 4px; background-color: #FF8484;"></span>');
+        $('#card__name').after('<span class="error" style="top: 364px; left: 24px; color: #FF8484;">Укажите цену товара</span>');
+        $('.card__price').css('border', "1px solid #FF8484");  
+    }
+});
+// -------------------------------------------------------------------
+
+
+// $(function(){
+// console.log('Jquery work');
+// });
